0c3a551996cc9f3c2b89b206f4713676ad44ff07
Mohammad Rahman <mbrahman@gmail.com>
2022-03-23 09:59:13 -0400

    finished created model column definitions

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,21 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+const fs = require('fs')
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
+    host: 'db.stippled.art',
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+      ssl: {
+        key: fs.readFileSync('./certs/client-key.cer'),
+        cert: fs.readFileSync('./certs/client-cert.cer'),
+        ca: fs.readFileSync('./certs/server-ca.cer')
       },
-    });
+    },
+  });
 
 module.exports = sequelize;
