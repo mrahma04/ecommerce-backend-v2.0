cdb57c4c4f04392ed31fc6e6484a920f8ad94853
Mohammad Rahman <mbrahman@gmail.com>
2022-03-25 17:21:57 -0400

    added error handling to product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -59,6 +59,7 @@ router.get('/:id', async (req, res) => {
     })
     if (!dbProductData) {
       res.status(404).json({ message: 'No product found with this id' })
+      return
     }
     res.json(dbProductData)
   } catch (err) {
